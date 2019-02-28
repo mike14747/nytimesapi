9c547e15c3246ad826f2e3fac96524845e624e11
mike4747 <mike4747@oh.rr.com>
2019-02-28 15:22:32 -0500

    working more updates

--- js/app.js
+++ js/app.js
@@ -4,16 +4,27 @@ $(document).ready(function () {
     var APIKey = "WfDOk3J8YKZ73khHDWso9JoLYFFyNwkr";
 
     var searchTerm = "";
-    var numRecords = "";
+    var numRecords = 0;
     var startDate = "";
     var endDate = "";
     var queryURL = "";
+    var newDate = "";
 
     $("#search-button").on("click", function () {
-        searchTerm = $("#search-term").val();
-        numRecords = $("#recordsToRetrieve").val();
-        startDate = $("#start-year").val() + "0101";
-        endDate = $("#end-year").val() + "0227";
+        searchTerm = $("#search-term").val().trim();
+        numRecords = $("#recordsToRetrieve").val().trim();
+        // startDate = $("#start-year").val().trim() + "0101";
+        // endDate = $("#end-year").val().trim() + "0228";
+        if ($("#start-year").val().trim() == "") {
+            startDate = "20190101";
+        } else {
+            startDate = $("#start-year").val().trim() + "0101";
+        }
+        if ($("#end-year").val().trim() == "") {
+            endDate = "20190228";
+        } else {
+            endDate = $("#end-year").val().trim() + "0228";
+        }
         queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&begin_date=" + startDate + "&end_date=" + endDate + "&api-key=" + APIKey;
 
         $.ajax({
@@ -22,14 +33,9 @@ $(document).ready(function () {
         }).then(function(response) {
 
             var r = response.response.docs;
-            // var r = response.response.docs[0].headline.main;
-            
-            console.log(r);
-            for (var i = 0; i < r.length; i++) {
-                $("#article-Placement").append("<div class='row'><div class='col-8'><a href='" + r[i].web_url + "' target='_blank'>" + r[i].headline.main + "</a></div><div class='col-4'>" + r[i].pub_date + "</div></div></p>");
-                // $("#article-Placement").append("<p>" + r[i].headline.main + "</p>");
-                // console.log(r + "[" + i + "].headline.main");
-                console.log(r[i].headline.main);
+
+            for (var i = 0; i < numRecords; i++) {
+                $("#article-Placement").append("<div class='row border-bottom mb-3'><div class='col-8 border-left p-2'><a href='" + r[i].web_url + "' target='_blank'>" + r[i].headline.main + "</a></div><div class='col-4'>" + r[i].pub_date + "</div></div>");
             }
 
             // headline.main, pub_date, web_url
